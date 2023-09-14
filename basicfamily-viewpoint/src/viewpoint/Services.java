f7161d7689a0b60a77e935f7b4dd19fd9cc8db17
Maged Elaasar <melaasar@gmail.com>
2023-09-14 08:23:35 -0700

    move cascade delete to menu

--- basicfamily-viewpoint/src/viewpoint/Services.java
+++ basicfamily-viewpoint/src/viewpoint/Services.java
@@ -3,12 +3,11 @@ package viewpoint;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-import io.opencaesar.oml.Entity;
 import io.opencaesar.oml.NamedInstance;
 import io.opencaesar.oml.Relation;
+import io.opencaesar.oml.util.OmlDelete;
 import io.opencaesar.oml.util.OmlDelete.CascadeDirection;
 import io.opencaesar.oml.util.OmlDelete.CascadeRule;
-import io.opencaesar.oml.util.OmlDelete;
 import io.opencaesar.oml.util.OmlRead;
 import io.opencaesar.rosetta.sirius.viewpoint.OmlServices;
 
@@ -33,13 +32,8 @@ public class Services {
     public void cascadeDelete(NamedInstance instance) {
     	var cascadeRules = new ArrayList<CascadeRule>();
     	
-    	Entity man = (Entity) OmlRead.getMemberByAbbreviatedIri(instance.getOntology(), "basicfamily:Man");
-    	Entity woman = (Entity) OmlRead.getMemberByAbbreviatedIri(instance.getOntology(), "basicfamily:Woman");
     	Relation parents = (Relation) OmlRead.getMemberByAbbreviatedIri(instance.getOntology(), "basicfamily:parents");
-    	Relation mother = (Relation) OmlRead.getMemberByAbbreviatedIri(instance.getOntology(), "basicfamily:mother");
-    	
     	cascadeRules.add(new CascadeRule(CascadeDirection.TARGET_TO_SOURCE, null, parents, null, "delete children"));
-    	cascadeRules.add(new CascadeRule(CascadeDirection.SOURCE_TO_TARGET, man, mother, woman, "delete mother"));
     	
     	var result = OmlDelete.cascadeDelete(instance, cascadeRules);
     	OmlDelete.recursiveDelete(result);
